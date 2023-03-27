82b06d323015c6066b464418170ad87c31d07381
Sergiu Abed s295149 <s295149@studenti.polito.it>
2023-03-27 16:14:23 +0200

    added cifar10.resnet entry in MODEL_PARAMS dictionary in file baseline_constants.py

--- models/baseline_constants.py
+++ models/baseline_constants.py
@@ -17,6 +17,7 @@ MAIN_PARAMS = {
 MODEL_PARAMS = {
     'cifar100.cnn': (0.01, 100),
     'cifar10.cnn': (0.01, 10),
+    'cifar10.resnet': (20, 10, "basic", False, 2, 64, "groupnorm"),
 }
 """dict: Model specific parameter specification"""
 
